b16a6acf61aed91bfe67b6eae3e88076cadb8114
Rafael_Madureira <rafinhakd4@gmail.com>
2022-06-08 20:52:07 -0300

    Add files via upload

---
+++ Exemplos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaliseComplexidade
+{
+    class Exemplos
+    {
+
+        public void MaximoVet1(int[] vetor, ref int maior, ref int menor)
+        {
+            maior = vetor[0];
+            menor = vetor[0];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] > maior)
+                {
+                    maior = vetor[i];
+                }
+                if (vetor[i] < menor)
+                {
+                    menor = vetor[i];
+                }
+            }
+        }
+        public void MaximoVet2(int[] vetor, ref int maior, ref int menor)
+        {
+            maior = vetor[0];
+            menor = vetor[0];
+            int elementoAzul;
+            int elementoVermelho;
+            for (int i = 0; i < vetor.Length; i+=2)
+            {
+                if (vetor[i] <vetor[i+1]) 
+                {
+                    elementoAzul=vetor[i];
+                    elementoVermelho = vetor[i + 1];
+
+                }
+                else
+                {
+                     elementoVermelho=vetor[i + 1];
+                     elementoAzul=vetor[i];
+                }
+                if (elementoAzul<menor)
+                {
+                    menor = elementoAzul;
+                }
+                if (elementoVermelho > maior)
+                {
+                    maior = elementoVermelho;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnaliseComplexidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = int.MaxValue;
+            int menor = int.MinValue;
+            Exemplos exemplo = new Exemplos();      
+            int[] vetor = new int[] {2,3,4,5,6,17,22,55 };
+            exemplo.MaximoVet2(vetor,ref maior,ref menor);            
+
+        }
+    }
+}
